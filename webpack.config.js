1fa982e5f6913fbe0551cd39242eb5159cd0bd68
Javier Martinez <me@javierm.com>
2016-09-01 20:02:33 +1200

    add sass support

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,13 @@ module.exports = {
           presets: ['react', 'es2015', 'stage-0'],
           plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
         }
-      }
+      },
+      {
+          test: /\.scss$/,
+          loader: 'style!css!sass?modules&localIdentName=[name]---[local]---[hash:base64:5]'
+      },
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
     ]
   },
   output: {
