f23457351d75ca71ba93e75cf4881138b3c8599e
Javier Martinez <me@javierm.com>
2016-12-01 00:11:46 +1300

    remove server to its own repo / enviroment

--- src/js/actions/heroesActions.js
+++ src/js/actions/heroesActions.js
@@ -1,13 +1,12 @@
-import axios from "axios"
-
+import axios from "axios";
 export function fetchHeroes(num) {
   return function(dispatch) {
-    axios.get("http://localhost:3003/api/characters/" + num)
+    axios.get("http://marvelheroes.api/characters/" + num)
       .then((response) => {
-        dispatch({type: "FETCH_HEROES_FULFILLED", payload: response.data})
+        dispatch({type: "FETCH_HEROES_FULFILLED", payload: response.data});
       })
       .catch((err) => {
-        dispatch({type: "FETCH_HEROES_REJECTED", payload: err})
-      })
-  }
+        dispatch({type: "FETCH_HEROES_REJECTED", payload: err});
+      });
+  };
 }
